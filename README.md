5b45972d2a8782c77c0cf3e102742de8eea1a837
Piotr Żurek <zurekdev@gmail.com>
2019-10-30 18:53:19 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # macopediatask
+
 Task for Macopedia
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <Shop />
+</template>
+
+<script>
+import Shop from './views/Shop.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Shop,
+  },
+};
+</script>
+
+<style lang="scss">
+*,
+*::after,
+::before {
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-rendering: optimizeLegibility;
+  scroll-behavior: smooth;
+}
+
+*::after,
+*::before {
+  box-sizing: inherit;
+}
+
+input,
+textarea,
+select,
+button,
+::placeholder {
+  font-family: inherit;
+}
+
+[v-cloak] {
+  display: hidden;
+}
+</style>
